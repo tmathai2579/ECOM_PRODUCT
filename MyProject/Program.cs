02a2ba1317e39fffb477cc5eb3ce430c758294ae
tmathai2579 <tmathai2579@conestogac.on.ca>
2024-06-02 22:57:04 -0400

    Add basic structure and product functionality
    - Create 'Product' class with properties for 'ProductID', 'ProductName', 'Price', and 'Stock'.
    - To initialize product details, implement a constructor in `Product` class.
    - Add methods to increase and decrease stock in the 'Product' class, with validation for stock reduction.

---
+++ MyProject/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace MyProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Product> products = new List<Product>();
+            
+        }
+    }
+}
